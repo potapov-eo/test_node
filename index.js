0f1c2137ad73584d4df581e3bcfedb16290d748d
Potapov <potapov.eo@gmail.com>
2022-03-14 00:23:17 +0300

    5

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const TelegramApi = require('node-telegram-bot-api')
+const http = require('http');
 
+const PORT = 5000;
+const server = http.createServer((req, res) => {})
+server.listen(PORT || 5050)
 const token = '5274353845:AAG--zZWI0AaebM_hOFEr4KmtN2Gxjop1B4'
 
 const bot = new TelegramApi(token, {polling: true})
-
 bot.on('message', async msg => {
     const text = msg.text;
     const chatId = msg.chat.id;
