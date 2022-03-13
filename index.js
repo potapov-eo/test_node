115b15031c5f25d8b3f1b800b94904f3f5e54606
Potapov <potapov.eo@gmail.com>
2022-03-13 23:53:49 +0300

    3

--- index.js
+++ index.js
@@ -4,19 +4,10 @@ const token = '5274353845:AAG--zZWI0AaebM_hOFEr4KmtN2Gxjop1B4'
 
 const bot = new TelegramApi(token, {polling: true})
 
-
 bot.on('message', async msg => {
     const text = msg.text;
     const chatId = msg.chat.id;
     console.log(msg)
     return bot.sendMessage(chatId, `сказал ${text}`);
-    try {
-        if (text === '/start') {
-            console.log("работает!!!!!!",msg )
-        }
-
-    } catch (e) {
-        return bot.sendMessage(chatId, 'Произошла какая то ошибочка!)');
-    }
 
 })
