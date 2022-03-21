ecb0ad2690f269ed1b821b4d8d79b8ee46d2024f
Potapov <potapov.eo@gmail.com>
2022-03-21 10:04:21 +0300

    12

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const TelegramApi = require('node-telegram-bot-api')
-const http = require('http');
-require('dotenv').config()
+const TelegramApi = require( 'node-telegram-bot-api' )
+const http = require( 'http' );
+const axios = require( 'axios' );
+require( 'dotenv' ).config()
 
-const server = http.createServer((req, res) => {})
-server.listen(process.env.PORT || 5050)
+const server = http.createServer( ( req, res ) => {} )
+server.listen( process.env.PORT || 5050 )
 
-const bot = new TelegramApi(process.env.token, {polling: true})
-bot.on('message', async msg => {
-    const text = msg.text;
-    const chatId = msg.chat.id;
-    console.log(msg)
-    return bot.sendMessage(chatId, `сказал ${text}`);
 
-})
+const bot = new TelegramApi( process.env.token, { polling: true } )
+
+bot.on( 'message', async msg => {
+
+    try {
+        const message_id = msg.message_id;
+        const chatId = msg.chat.id;
+        const text = msg.text;
+        if ( text === '/usd' ) {
+            const res = await axios( 'https://www.nbrb.by/api/exrates/rates/840?parammode=1' )
+            await bot.deleteMessage( chatId, message_id )
+            await bot.sendMessage( chatId, `курс  ${res.data.Cur_Abbreviation} : ${res.data.Cur_OfficialRate} бел.руб. ` )
+            /*.then(e=>bot.deleteMessage( chatId, message_id ))*/
+
+        }
+        //return bot.sendMessage( chatId, `сказал ${text}` );
+        console.log( 'aaaaaaaa' )
+    } catch (e) {
+        console.log( e )
+    }
+
+} )
